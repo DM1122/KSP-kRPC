e3fbb47805661cc4611a79ae1458b88e37265d3c
David Maranto <d.maranto1122@gmail.com>
2022-04-13 20:27:32 -0400

    Remove hook packages from local installation

---
+++ main/sympy_addons.py
@@ -0,0 +1,28 @@
+# stdlib
+import warnings
+
+# external
+import sympy as sy
+
+
+def solve_eq(eq, symbol):
+    soln = sy.solveset(f=eq, symbol=symbol)
+    print(sy.latex(soln))
+    if len(soln.args) == 0:
+        raise ValueError(f"No solutions exists:\n{soln}")
+    elif len(soln.args) > 1:
+        warnings.warn(f"More than one solution exists:\n{soln}")
+    else:
+        soln = soln.args[0]
+
+    return sy.Eq(symbol, soln)
+
+
+def L(f):
+    # t, s = sy.symbols("t, s")
+    return sy.laplace_transform(f, t, s, noconds=True)
+
+
+def L_inv(F):
+    # t, s = sy.Symbol("t, s")
+    return sympy.inverse_laplace_transform(F, s, t)
