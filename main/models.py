b92edafa455c592fb3c13e2729dc672a158d5278
David Maranto <d.maranto1122@gmail.com>
2022-04-16 15:06:09 -0400

    Add simulator

---
+++ main/models.py
@@ -0,0 +1,33 @@
+# external
+import numpy as np
+
+
+class ConstantMassModel:
+    def __init__(self, x_0, m, b, g):
+        self.x_0 = x_0
+        self.x = x_0
+        self.m = m
+        self.b = b
+        self.g = g
+
+    def plant(self, u, dt):
+        # alloc
+        dx = np.zeros((len(self.x), 1))
+
+        # process
+        dx[0] = self.x[1]
+        dx[1] = -self.b / self.m * self.x[1] - self.g + 1 / self.m * u
+
+        # update
+        self.x = self.x + dx * dt
+
+        return self.x
+
+    def reset(self):
+        self.x = self.x_0
+
+    def __call__(self, u, dt):
+
+        x = self.plant(u, dt)
+
+        return x
