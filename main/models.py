77ecfb017c4eed8a550e19fad003ceddecb1e299
David Maranto <d.maranto1122@gmail.com>
2022-04-25 13:39:26 -0400

    Change constant mass model to use throttle value as input

--- main/models.py
+++ main/models.py
@@ -2,15 +2,14 @@
 import numpy as np
 
 
-
-
 class ConstantMassModel:
-    def __init__(self, x_0, m, b, g):
+    def __init__(self, x_0, m, b, g, F_t_max):
         self.x_0 = x_0
         self.x = x_0
         self.m = m
         self.b = b
         self.g = g
+        self.F_t_max = F_t_max
 
     def plant(self, u, dt):
         # alloc
@@ -18,7 +17,8 @@ class ConstantMassModel:
 
         # process
         dx[0] = self.x[1]
-        dx[1] = -self.b / self.m * self.x[1] - self.g + 1 / self.m * u
+        dx[1] = self.F_t_max * u / self.m - self.b * self.x[1] / self.m - self.g
+        # dx[1] = -self.b / self.m * self.x[1] - self.g + 1 / self.m * u*self.F_t_max
 
         # update
         self.x = self.x + dx * dt
